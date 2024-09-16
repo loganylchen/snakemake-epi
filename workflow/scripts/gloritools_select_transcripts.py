103e48a0f50869d92bed986c488b5d93a8c42669
logan <yuelong.chen.btr@gmail.com>
2024-09-16 07:45:30 +0000

    Refactor gloritools_select_transcripts.py to include output directory creation and print statement

--- workflow/scripts/gloritools_select_transcripts.py
+++ workflow/scripts/gloritools_select_transcripts.py
@@ -5,6 +5,7 @@ import os
 import re
 from collections import defaultdict,OrderedDict
 import pandas as pd
+import pysam
 
 
 sys.stdout = open(snakemake.log.log,'w') 
@@ -13,7 +14,10 @@ sys.stderr= open(snakemake.log.err, 'w')
 annotate_df = pd.read_csv(snakemake.input.anno,sep='\t')
 output_list=snakemake.output.longest_transcript
 
-os.makedirs(os.path.dirname(output_list))
+os.makedirs(os.path.dirname(output_list),exist_ok=True)
+with pysam.FastaFile(snakemake.input.transcriptome_reference) as fasta:
+    transcripts = fasta.references
+
 
 print('selecting longest transcripts')
 # NF564	ENST00000339282	19	-	12525373	12551482	12526449,12528304,12528570,12551330	12527916,12528364,12528696,12551332	4	12525373,12528304,12528570,12551330	12527916,12528364,12528696,12551482	2885	ENSG00000249709	protein_coding	12	1659
@@ -22,7 +26,7 @@ print('selecting longest transcripts')
 # ZNF564	ENST00000416136	19	-	12548713	12551434	12548803,12551330	12548862,12551332	2	12548713,12551330	12548862,12551434	255	ENSG00000249709	protein_coding	1	63
 # ZNF564	ENST00000465410	19	-	12550242	12551432	.	.	2	12550242,12551330	12550652,12551432	514	ENSG00000249709	retained_intron	1	0
 
-
+annotate_df = annotate_df.loc[annotate_df['transcript_id'].isin(list(transcripts))]
 selected_df = annotate_df.sort_values('transcript_length', ascending=False).drop_duplicates('gene_id')
 selected_df.to_csv(f'{output_list}.tsv',sep='\t',index=False)
 selected_df['transcript_id'].to_csv(output_list,sep='\t',index=False,header=False)
